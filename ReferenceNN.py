8f000806fb476e53e2e0e08f1299fced249db0cb
Rohan Sumant <r.s.sumant@gmail.com>
2024-04-06 17:29:53 -0700

    Adding a reference NN

---
+++ ReferenceNN.py
@@ -0,0 +1,12 @@
+# https://github.com/miloharper/simple-neural-network/blob/master/short_version.py
+
+from numpy import exp, array, random, dot
+training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
+training_set_outputs = array([[0, 1, 1, 0]]).T
+random.seed(1)
+synaptic_weights = 2 * random.random((3, 1)) - 1
+print(synaptic_weights)
+for iteration in range(10000):
+    output = 1 / (1 + exp(-(dot(training_set_inputs, synaptic_weights))))
+    synaptic_weights += dot(training_set_inputs.T, (training_set_outputs - output) * output * (1 - output))
+print (1 / (1 + exp(-(dot(array([1, 0, 0]), synaptic_weights)))))
